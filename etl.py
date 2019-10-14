f322ee55af8f9dd02cff78a499f260ecf5d317b1
Shubhang Malviya <shubhang.malviya@daffodilsw.com>
2019-10-14 23:32:14 +0530

    1. Make sql_queries more readable by writing them in multiple lines. 2. Restructured the etl script to refactor artist insert before the song

--- etl.py
+++ etl.py
@@ -17,14 +17,14 @@ def process_song_file(cur, filepath):
     # open song file
     df = pd.read_json(filepath, lines=True)
 
-    # insert song record
-    song_data = list(df[["song_id", "title", "artist_id", "year", "duration"]].values[0])
-    cur.execute(song_table_insert, song_data)
-
     # insert artist record
     artist_data = list(df[['artist_id', 'artist_name', 'artist_location', 'artist_latitude', \
                            'artist_longitude']].values[0])
     cur.execute(artist_table_insert, artist_data)
+
+    # insert song record
+    song_data = list(df[["song_id", "title", "artist_id", "year", "duration"]].values[0])
+    cur.execute(song_table_insert, song_data)
 
 
 def process_log_file(cur, filepath):
